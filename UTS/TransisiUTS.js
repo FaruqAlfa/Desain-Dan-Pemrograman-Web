587b84c8bb9cb63bda30daa473050cc3f97fce57
nicoco1173 <rikurunico@gmail.com>
2022-10-18 10:31:15 +0700

    Fix Bug

--- UTS/TransisiUTS.js
+++ UTS/TransisiUTS.js
@@ -1,11 +1,14 @@
-const navbar = document.querySelector(".navbar")
-window.addEventListener("scroll", function(e){
-    console.log(window.scrollY);
-    if (this.scrollY > 100) {
-        navbar.classList.add("bg-blue")
-        navbar.classList.remove("bg-transparent")
-    } else {
-        navbar.classList.remove("bg-blue")
-    
-    }
+const navbar = document.querySelector(".navbar");
+const links = document.querySelectorAll(".navbar-item a");
+
+window.addEventListener("scroll", function (e) {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    navbar.classList.add("bg-blue");
+    links.forEach((link) => (link.style.color = "#fff"));
+  } else {
+    navbar.classList.remove("bg-blue");
+    links.forEach((link) => {
+      link.style.color = "black";
+    });
+  }
 });
